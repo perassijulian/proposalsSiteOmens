90e5a6fc73c913b3262aa1609c7d335c0ff05245
Julian Perassi <perassijulian@gmail.com>
2022-01-10 19:04:06 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Import ThirdWeb
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// Include what chains you wanna support.
+// 4 = Rinkeby.
+const supportedChainIds = [4];
+
+// Include what type of wallet you want to support.
+// In this case, we support Metamask which is an "injected wallet".
+const connectors = {
+  injected: {},
+};
+
+// Finally, wrap App with ThirdwebWeb3Provider.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
