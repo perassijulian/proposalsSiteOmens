56d409fc99ac6597ef5750fbeefcaa6a9f65ac10
Julian Perassi <perassijulian@gmail.com>
2022-01-15 18:45:44 -0300

    add execute logic

--- src/components/Treasury.js
+++ src/components/Treasury.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Treasury(props) {
     // A fancy function to shorten someones wallet address, no need to show the whole thing. 
+    const [canExec, setCanExec] = useState("");
+
     const shortenAddress = (str) => {
         return str.substring(0, 6) + "..." + str.substring(str.length - 4);
     };
 
+    const optionDisplay = props.proposals.map((item) => {
+        let showing = item.description.slice(0, 10)
+        return(<option value={item.proposalId}>{showing}</option>)
+    })
+
+
+    async function handleChange(event){
+        setCanExec(await props.voteModule.canExecute(event.target.value))
+    }
+    console.log('afuera', canExec)
     return(
         <div>  
           <p>Treasury</p>
@@ -13,6 +25,17 @@ export default function Treasury(props) {
           <h4>TODO: button to check if proposal is ready to execute</h4>
           <h4>TODO: button to execute proposal</h4>
           <h4>TBD: register of transfers?</h4>
+          <div>
+            <select
+                className="execute--proposals" 
+                onChange={handleChange}
+            >
+                <option value="">-- SELECT --</option>
+                {optionDisplay}
+            </select>
+            <button>{canExec? "EXECUTE" : "CAN'T EXECUTE"}</button>
+          </div>
+          
         </div>
     )
 }
